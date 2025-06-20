af99adb1295d841a454154ea103969764448477e
Cristian Beraha <cristian@cloudops.works>
2025-06-20 14:22:44 -0300

    Update README and module configuration for enhanced DynamoDB support
    Expanded `README.yaml` with detailed examples and instructions. Enhanced Terraform module configuration to support additional options, including autoscaling, encryption, multi-region replicas, and table imports. Upgraded AWS provider version and improved file headers to reflect license and links.

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
 ##
-# (c) 2024 - Cloud Ops Works LLC - https://cloudops.works/
-#            On GitHub: https://github.com/cloudopsworks
-#            Distributed Under Apache v2.0 License
+# (c) 2021-2025
+#     Cloud Ops Works LLC - https://cloudops.works/
+#     Find us on:
+#       GitHub: https://github.com/cloudopsworks
+#       WebSite: https://cloudops.works
+#     Distributed Under Apache v2.0 License
 #
 
 locals {
@@ -9,27 +12,35 @@ locals {
 }
 
 module "this" {
-  source                             = "terraform-aws-modules/dynamodb-table/aws"
-  version                            = "~> 4.0"
-  create_table                       = true
-  name                               = local.table_name
-  attributes                         = try(var.db.attributes, [])
-  autoscaling_defaults               = try(var.db.autoscaling_defaults, {})
-  autoscaling_enabled                = try(var.db.autoscaling_enabled, false)
-  billing_mode                       = try(var.db.capacity.billing_mode, "PAY_PER_REQUEST")
-  read_capacity                      = try(var.db.capacity.read, 0)
-  write_capacity                     = try(var.db.capacity.write, 0)
-  deletion_protection_enabled        = try(var.db.deletion_protection, false)
-  hash_key                           = try(var.db.hash_key, null)
-  range_key                          = try(var.db.range_key, null)
-  point_in_time_recovery_enabled     = try(var.db.recovery.enabled, false)
-  replica_regions                    = try(var.db.replicas.regions, [])
-  server_side_encryption_enabled     = try(var.db.encryption.enabled, false)
-  server_side_encryption_kms_key_arn = try(var.db.encryption.kms_key_arn, null)
-  stream_enabled                     = try(var.db.stream.enabled, false)
-  stream_view_type                   = try(var.db.stream.view_type, null)
-  table_class                        = try(var.db.table_class, null)
-  ttl_enabled                        = try(var.db.ttl.enabled, false)
-  ttl_attribute_name                 = try(var.db.ttl.attribute_name, null)
-  tags                               = local.all_tags
+  source                                = "terraform-aws-modules/dynamodb-table/aws"
+  version                               = "~> 4.0"
+  create_table                          = true
+  name                                  = local.table_name
+  attributes                            = try(var.db.attributes, [])
+  autoscaling_defaults                  = try(var.db.autoscaling_defaults, var.db.autoscaling.defaults, {})
+  autoscaling_enabled                   = try(var.db.autoscaling_enabled, var.db.autoscaling.enabled, false)
+  autoscaling_indexes                   = try(var.db.autoscaling_indexes, var.db.autoscaling.indexes, false)
+  autoscaling_read                      = try(var.db.autoscaling.read, {})
+  autoscaling_write                     = try(var.db.autoscaling.write, {})
+  billing_mode                          = try(var.db.capacity.billing_mode, "PAY_PER_REQUEST")
+  read_capacity                         = try(var.db.capacity.read, 0)
+  write_capacity                        = try(var.db.capacity.write, 0)
+  deletion_protection_enabled           = try(var.db.deletion_protection, false)
+  hash_key                              = try(var.db.hash_key, null)
+  range_key                             = try(var.db.range_key, null)
+  point_in_time_recovery_enabled        = try(var.db.recovery.enabled, false)
+  point_in_time_recovery_period_in_days = try(var.db.recovery.period_in_days, null)
+  replica_regions                       = try(var.db.replicas.regions, [])
+  server_side_encryption_enabled        = try(var.db.encryption.enabled, false)
+  server_side_encryption_kms_key_arn    = try(var.db.encryption.kms_key_arn, null)
+  stream_enabled                        = try(var.db.stream.enabled, false)
+  stream_view_type                      = try(var.db.stream.view_type, null)
+  table_class                           = try(var.db.table_class, null)
+  ttl_enabled                           = try(var.db.ttl.enabled, false)
+  ttl_attribute_name                    = try(var.db.ttl.attribute_name, null)
+  global_secondary_indexes              = try(var.db.global_secondary_indexes, [])
+  ignore_changes_global_secondary_index = try(var.db.ignore_changes_global_secondary_index, false)
+  local_secondary_indexes               = try(var.db.local_secondary_indexes, [])
+  import_table                          = try(var.db.import_table, false)
+  tags                                  = local.all_tags
 }
